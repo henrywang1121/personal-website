532e0286964b1b57409630533d4eef1b7d0bdc7b
ET <et@ETs-MacBook.local>
2019-02-03 10:02:23 -0800

    typed

--- js/script.js
+++ js/script.js
@@ -4,4 +4,13 @@ $(document).ready(function(){
         play: 2500,
         pagination: false
     });
+
+    var type = new Typed(".typed",{
+        strings: ["M.S. Computer Information Systems", 
+                    "Boston University", "New Grad"],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+    });
 });
